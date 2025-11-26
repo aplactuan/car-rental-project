3511e7c7935ef8793dafb62eee6709a06e2867e8
Adrian Lactuan <aplactuan@gmail.com>
2025-11-26 21:26:54 +0800

    add test case for only authorize user can add a car

--- tests/Feature/AddCarTest.php
+++ tests/Feature/AddCarTest.php
@@ -6,6 +6,23 @@ use function Pest\Laravel\assertDatabaseHas;
 
 uses(RefreshDatabase::class);
 
+test('it cannot add a car if user is not login', function () {
+    $this->withoutExceptionHandling();
+    $payload = [
+        'make' => 'Toyota',
+        'model' => 'Raize',
+        'year' => 2020,
+        'mileage' => 5000,
+        'type' => 'SUV',
+        'number_of_seats' => 5,
+        'plate_number' => 'IJC2912',
+    ];
+
+    $response = $this->post('/api/v1/cars', $payload);
+
+    $response->assertStatus('401');
+});
+
 test('it can add a car thru api', function () {
     $this->withoutExceptionHandling();
     $payload = [
