da762a7e80e0a9f3abbdb56b21695f7b9600432d
Adrian <aplactuan@gmail.com>
2026-05-29 11:17:56 +0800

    fix migration issue in prod

--- database/migrations/2026_05_20_205335_add_price_to_bookings_table.php
+++ database/migrations/2026_05_20_205335_add_price_to_bookings_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('bookings', function (Blueprint $table) {
-            $table->unsignedInteger('price')->after('note');
+            $table->unsignedInteger('price')->default(0)->after('note');
         });
     }
 
