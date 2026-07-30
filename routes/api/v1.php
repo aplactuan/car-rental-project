060564942205bcecdadc237f5707fb7f6bc0fb18
Adrian <aplactuan@gmail.com>
2026-07-30 21:08:29 +0800

    route for delete endpoint

--- routes/api/v1.php
+++ routes/api/v1.php
@@ -38,6 +38,7 @@ use App\Http\Controllers\V1\Drivers\ShowDriverImportController;
 use App\Http\Controllers\V1\Drivers\SingleDriverController;
 use App\Http\Controllers\V1\Drivers\UpdateDriverController;
 use App\Http\Controllers\V1\Invoices\AddInvoiceController;
+use App\Http\Controllers\V1\Invoices\DeleteInvoiceController;
 use App\Http\Controllers\V1\Invoices\ListInvoicesController;
 use App\Http\Controllers\V1\Invoices\ShowInvoiceController;
 use App\Http\Controllers\V1\Invoices\UpdateInvoiceController;
@@ -99,6 +100,7 @@ Route::prefix('v1')->middleware('auth:sanctum')->group(function () {
     Route::get('/purchase-orders/{purchaseOrder}/invoices', ListInvoicesController::class);
     Route::get('/purchase-orders/{purchaseOrder}/invoices/{invoice}', ShowInvoiceController::class);
     Route::put('/purchase-orders/{purchaseOrder}/invoices/{invoice}', UpdateInvoiceController::class);
+    Route::delete('/purchase-orders/{purchaseOrder}/invoices/{invoice}', DeleteInvoiceController::class);
 
     Route::get('/billing/summary', BillingSummaryController::class);
     Route::get('/bills', ListBillsController::class);
